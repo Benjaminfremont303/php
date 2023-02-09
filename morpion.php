39d05c19584773b82662a8da508b8c7e1b2c317f
Benjaminfremont303 <124568438+Benjaminfremont303@users.noreply.github.com>
2023-02-09 18:29:17 +0000

    1

--- morpion.php
+++ morpion.php
@@ -1,60 +1,43 @@
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Tic Tac Toe</title>
 </head>
-<body>
-    <?php 
-       // print_r($_GET);  
-        $x=$_GET['x'];
-
-        $y=$_GET['y'];
-
-        $j=$_GET['j']; 
-
-        if($j==1) {
-            $j = 2;
-        } else {
-            $j = 1;
-        }
-
-    
-    ?>
+<body> 
     <table>
-    <tr> 
-        <td>
-            <a href="?x=1&y=1&j=j<?php echo $j ?>">
-                <?php 
-                    if($x==1 && $y==1 && $j==1) {
-                        echo "O";
+        <?php
+        $x=$_GET["x"];
+        $y=$_GET["y"];
+
+            for ($i=0;$i<3;$i++){
+                echo '<tr>';
+                for($j=0;$j<3;$j++){
+                    echo '<td>' ;
+                    echo "<a href='?x=";
+                    echo $i;
+                    echo "&y=";
+                    echo $j;
+                    echo "'>";
+                    if ($x==$i && $j==$y){
+                        echo $i.'X'.$j;
                     }
-                    else if ($x==1 && $y==1 && $j==0){
-                        echo "x";
-                    
+                    else{
+                        echo $i.'_'.$j;
                     }
-                    else {
-                        echo "_";
-                    }
-                ?>
-            </a>
-        </td>
-        <td>
-            <a href="?x=1&y=2">_</a>
-        </td>
-        <td>
-            <a href="?x=1&y=3">_</a>
-        </td>
-   <?php phpinfo() ?>
-    </tr>
+
+                    echo "</a>";
+                    echo '</td>';
+                }
+                echo '</tr>';
+            }
+            print_r($_GET);
+        ?>
     </table>
+
 </body>
 </html>
-
-
-
-
-
