f2912c95090da1c56ad18ccbdb079de3864d7667
Benjaminfremont303 <124568438+Benjaminfremont303@users.noreply.github.com>
2023-02-13 16:06:38 +0100

    exo factorielle, carré, addi

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+     function carre($n){
+     return $n*$n;
+}
+
+
+
+function factorielle($nombre){
+    $fact = 1;
+    for ($i = 1; $i <= $nombre; $i++) {
+    $fact = $fact * $i;
+
+    }
+    return $fact;
+}
+
+
+function add ($n) {
+   return (($n + 1) * $n) /2;
+}
+ 
+
+echo "le carré est". ' '.carre(5). "\n";
+
+echo "la somme est". ' ' .add(100). "\n";
+
+
+
+echo "la factorielle est ". ' ' .factorielle(8);
+
+
+
+
+
+?>
